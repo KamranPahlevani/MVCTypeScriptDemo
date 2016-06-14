4ec4b07bf9597c419071cc4b496a13813b36aa1d
masoudparsa <m.parsanejad@gmail.com>
2016-06-14 13:37:20 +0430

    test

---
+++ MVCTypeScriptDemo/Scripts/Main.js
@@ -0,0 +1,25 @@
+﻿
+// Module
+var Shapes;
+(function (Shapes) {
+    // Class
+    var Point = (function () {
+        function Point(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        // Interface Member
+        Point.prototype.getDist = function () {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        };
+
+        Point.origin = new Point(0, 0);
+        return Point;
+    })();
+    Shapes.Point = Point;
+})(Shapes || (Shapes = {}));
+
+// Local variables
+var p = new Shapes.Point(3, 4);
+var dist = p.getDist();
+//# sourceMappingURL=Main.js.map
